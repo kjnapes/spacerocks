dd7b2e88dfe5f30c0a1c5899a7c354723c33a237
Kevin Napier <kjnapier@umich.edu>
2024-12-12 18:41:41 -0500

    Testing the simulation reimplementation.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
-use spacerocks::{SpaceRock, Time};
+use spacerocks::{SpaceRock, Time, SpiceKernel, Simulation};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rock = SpaceRock::from_horizons("Arrokoth", &Time::now(), "j2000", "ssb")?;
-    println!("{}", rock);
-    println!("---------------------------");
-    // rock.set_mass(1.0);
+
+    let spice_root = "/Users/kjnapier/data/spice";
+
+    // define the epoch
+    let epoch = Time::now();
+
+    // load spice kernels
+    let mut kernel = SpiceKernel::new();
+    kernel.load(format!("{}/de440s.bsp", spice_root).as_str())?;
+    kernel.load(format!("{}/latest_leapseconds.tls", spice_root).as_str())?;
+
+    // load earth from spice
+    // let earth = SpaceRock::from_spice("EarTh", &epoch, "EcLiPj2000", "sSb")?;
+    // println!("{}", earth);
+
+    // load arrokoth from spice
+    let rock = SpaceRock::from_horizons("Arrokoth", &epoch, "j2000", "ssb")?;
     // println!("{}", rock);
+
+    // create a simulation and add arrokoth
+    let mut sim = Simulation::giants(&epoch, "ECLIPJ2000", "SSB")?;
+    sim.add(rock)?; 
+
+    sim.integrate(&(epoch + 1.0));
+    for rock in sim.particles {
+        println!("{}", rock);
+    }
 
 
     Ok(())
